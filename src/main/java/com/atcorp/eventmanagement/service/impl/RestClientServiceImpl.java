516e7ddfd300cc921a5387d15a139c20054472d4
Ajinkya Tanksale <tanksale.ajinkya@gmail.com>
2025-08-31 00:54:57 +0530

    Added docker file

--- src/main/java/com/atcorp/eventmanagement/service/impl/RestClientServiceImpl.java
+++ src/main/java/com/atcorp/eventmanagement/service/impl/RestClientServiceImpl.java
@@ -3,6 +3,7 @@ package com.atcorp.eventmanagement.service.impl;
 import com.atcorp.eventmanagement.dto.userdetails.RestUserDetailsResponse;
 import com.atcorp.eventmanagement.service.RestClinetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,8 +14,11 @@ public class RestClientServiceImpl implements RestClinetService {
     @Autowired
     RestTemplate restTemplate;
 
+    @Value("${usermanagement.url}")
+    String userManagementUrl;
+
     public RestUserDetailsResponse getUserDetailsByUserId (long userId) {
-        ResponseEntity<RestUserDetailsResponse> response = restTemplate.getForEntity("http://localhost:8081/internal/getUserInfo/" + userId, RestUserDetailsResponse.class);
+        ResponseEntity<RestUserDetailsResponse> response = restTemplate.getForEntity(userManagementUrl + "internal/getUserInfo/" + userId, RestUserDetailsResponse.class);
         return response.getBody();
     }
 }
